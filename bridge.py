ec1788cd484ca36c087157d34a1c97e4b28d065e
Frepellant <33057458+Freepellant@users.noreply.github.com>
2026-02-19 19:03:25 +1100

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -1,20 +1,25 @@
 import paho.mqtt.client as mqtt
-from Adafruit_IO import Client
 import json
 import os
+import urllib.request
 
-# Adafruit IO settings
 ADAFRUIT_USERNAME = os.environ.get("ADAFRUIT_USERNAME")
 ADAFRUIT_KEY = os.environ.get("ADAFRUIT_KEY")
 
-# Meshtastic MQTT settings
 MESH_BROKER = "mqtt.meshtastic.org"
 MESH_USER = "meshdev"
 MESH_PASS = "large4cats"
 MESH_TOPIC = "msh/ANZ/2/json/LongFast/!91306040"
 
-# Adafruit IO client
-aio = Client(ADAFRUIT_USERNAME, ADAFRUIT_KEY)
+def send_to_adafruit(lat, lon):
+    url = "https://io.adafruit.com/api/v2/{}/feeds/location/data".format(ADAFRUIT_USERNAME)
+    payload = json.dumps({"value": "0", "lat": lat, "lon": lon}).encode()
+    req = urllib.request.Request(url, data=payload, headers={
+        "X-AIO-Key": ADAFRUIT_KEY,
+        "Content-Type": "application/json"
+    })
+    urllib.request.urlopen(req)
+    print("Sent location: {}, {}".format(lat, lon))
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to Meshtastic MQTT")
@@ -26,9 +31,7 @@ def on_message(client, userdata, msg):
         if data.get("type") == "position":
             lat = data["payload"]["latitude_i"] / 10000000
             lon = data["payload"]["longitude_i"] / 10000000
-            location = "{},{}".format(lat, lon)
-            aio.send("location", location)
-            print("Sent location: {}".format(location))
+            send_to_adafruit(lat, lon)
     except Exception as e:
         print("Error: {}".format(e))
 
@@ -38,3 +41,6 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.connect(MESH_BROKER, 1883, 60)
 client.loop_forever()
+```
+
+
